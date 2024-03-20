73b40c917e3c065f1390b13454352ca5885f849c
Tero Tervala <tero.tervala@unikie.com>
2024-03-20 20:55:27 +0200

    Add systemd suspend preventions
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -63,6 +63,13 @@
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.autoSuspend = false;
 
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   # Configure keymap in X11
   services.xserver = {
     layout = "fi";
