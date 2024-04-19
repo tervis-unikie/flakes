65bfee247b46d5c74541aac4b6e1d56b5c3f6f42
Tero Tervala <tero.tervala@unikie.com>
2024-04-19 09:46:41 +0300

    Reorganize hosts directory contents

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,1 +1,26 @@
-(import ./nixos)
+# SPDX-License-Identifier: MIT
+{
+  lib,
+  inputs,
+  nixpkgs,
+  user,
+  ...
+}: let
+  system = "x86_64-linux";
+
+  pkgs = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+
+  lib = nixpkgs.lib;
+in {
+  tervis-servu = lib.nixosSystem {
+    inherit system;
+    specialArgs = {inherit inputs user;};
+    modules = [
+      ./tervis-servu/hardware-configuration.nix
+      ./tervis-servu/configuration.nix
+    ];
+  };
+}
