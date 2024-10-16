9542e86db335de6a15c93e1fea19a2868f668fd2
Tero Tervala <tero.tervala@unikie.com>
2024-10-16 13:45:33 +0300

    Removed cache.vedenemo.dev

--- hosts/tervis-servu/configuration.nix
+++ hosts/tervis-servu/configuration.nix
@@ -120,14 +120,12 @@
   nix.settings.trusted-users = [ "root" "@wheel" ];
 
   nix.settings.trusted-substituters = [
-    "https://cache.vedenemo.dev"
     "https://ghaf-dev.cachix.org"
     "https://cache.nixos.org/"
     "https://cache.ssrcdevops.tii.ae"
   ];
 
   nix.settings.trusted-public-keys = [
-    "cache.vedenemo.dev:8NhplARANhClUSWJyLVk4WMyy1Wb4rhmWW2u8AejH9E="
     "ghaf-dev.cachix.org-1:S3M8x3no8LFQPBfHw1jl6nmP8A7cVWKntoMKN3IsEQY="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "cache.ssrcdevops.tii.ae:oOrzj9iCppf+me5/3sN/BxEkp5SaFkHfKTPPZ97xXQk="
